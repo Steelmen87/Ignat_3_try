8b2d120d30ffd0cacbaa88d57f317530e752961b
Steel <Steelmen87@bk.ru>
2023-05-20 17:34:12 +0300

    hw12 done+-

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,27 @@
+export type initStateType = {
+    themeId: number
+}
+
+
 const initState = {
     themeId: 1,
 }
-
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type actionType = changeThemeIdType
+export const themeReducer = (state: initStateType = initState, action: actionType): initStateType => { // fix any
     switch (action.type) {
-        // дописать
+        case "SET_THEME_ID": {
+            return {
+                ...state,
+                themeId: action.id
+            }
+        }
 
         default:
             return state
     }
 }
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type changeThemeIdType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+export const changeThemeId = (id: number): changeThemeIdType => ({type: 'SET_THEME_ID', id}) // fix any
